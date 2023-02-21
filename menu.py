7587c8b52dcbf5f7ce360dce57e258385408e05f
Daniel Molina Rivera <dmolina0310@gmail.com>
2023-02-20 21:34:39 -0300

    venta de productos

--- menu.py
+++ menu.py
@@ -1,8 +1,8 @@
 import json
 import os
 from colorama import Fore, Back, Style
-#from ventas import crear_cliente, mostrar_num_clientes, eliminar_cliente
 from bodega import crear_producto, mostrar_productos, uni_disp_producto, uni_disp_producto_sel, mostrar_productos_cant, actualizar_stock
+from ventas import compra_producto
 
 # Configurar Colorama
 Fore.GREEN = '\033[92m'
@@ -15,7 +15,7 @@ Back.RESET = '\033[0m'
 
 #Menú Clientes
 # Función para administrar clientes
-"""def control_ventas():
+def control_ventas():
     os.system("cls" if os.name == "nt" else "clear")
     while True:
         print(Back.GREEN + Fore.WHITE + "************************" + Back.RESET)
@@ -28,19 +28,52 @@ Back.RESET = '\033[0m'
         opcion = input(Fore.YELLOW + "Selecciona una opción: " + Fore.RESET)
 
         if opcion == "1":
-            crear_cliente()
+            pass
         elif opcion == "2":
-            mostrar_num_clientes()
+            pass
         elif opcion == "3":
-            eliminar_cliente()
+            compra_producto()
         elif opcion == "0":
             break
         else:
-            print(Back.RED + Fore.WHITE + "Opción inválida" + Back.RESET + Fore.RESET) """
+            print(Back.RED + Fore.WHITE + "Opción inválida" + Back.RESET + Fore.RESET)
 
 #Menú Control Bodega
 # Función para Control de Bodega
 def control_bodega():
+    os.system("cls" if os.name == "nt" else "clear")
+    while True:
+        print(Back.GREEN + Fore.WHITE + "************************" + Back.RESET)
+        print(Back.GREEN + Fore.WHITE + "**   Control Bodega   **" + Back.RESET)
+        print(Back.GREEN + Fore.WHITE + "************************" + Back.RESET)
+        print(Fore.GREEN + "1. Crear nuevo Producto" + Fore.RESET)
+        print(Fore.GREEN + "2. Actualizar Stock" + Fore.RESET)
+        print(Fore.GREEN + "3. Unidades Disponibles x Producto" + Fore.RESET)
+        print(Fore.GREEN + "4. Unidades Disponibles x Producto Seleccionado" + Fore.RESET)
+        print(Fore.GREEN + "5. Listar Todos los Productos" + Fore.RESET)
+        print(Fore.GREEN + "6. Listar Todos los Productos con + de Cantidad Seleccionada" + Fore.RESET)
+        print(Fore.GREEN + "0. Volver" + Fore.RESET)
+        opcion = input(Fore.YELLOW + "Selecciona una opción: " + Fore.RESET)
+
+        if opcion == "1":
+            crear_producto()
+        elif opcion == "2":
+            actualizar_stock()
+        elif opcion == "3":
+            uni_disp_producto()
+        elif opcion == "4":
+            uni_disp_producto_sel()
+        elif opcion == "5":
+            mostrar_productos()
+        elif opcion == "6":
+            mostrar_productos_cant()
+            
+        elif opcion == "0":
+            break
+        else:
+            print(Back.RED + Fore.WHITE + "Opción inválida" + Back.RESET + Fore.RESET)
+
+def control_venta():
     os.system("cls" if os.name == "nt" else "clear")
     while True:
         print(Back.GREEN + Fore.WHITE + "************************" + Back.RESET)
@@ -93,7 +126,7 @@ while True:
     if opcion == "1":
         control_bodega()
     elif opcion == "2":
-        pass
+        control_ventas()
     elif opcion == "0":
         break
     else:
---
+++ ventas.py
@@ -0,0 +1,69 @@
+import json
+import os
+import time
+from colorama import Fore, Back
+# Cargar los productos desde el archivo externo
+with open("cliente.json", "r") as archivo:
+    clientes = json.load(archivo)
+# Cargar los productos desde el archivo externo
+with open("bodega.json", "r") as archivo:
+    productos = json.load(archivo)
+
+def valida_stock(id, cant):
+    for producto in productos:
+        if producto["id"] == id:
+            if producto["stock"] >= cant:
+                return True
+            else:
+                return False
+            
+def valida_numero(num):
+    return True if num.isdigit() else False
+
+def valida_venta(num):
+    return True if num.isdigit() else False
+
+def compra_producto():
+    cantOk = True
+    os.system("cls" if os.name == "nt" else "clear")
+
+    print(Back.GREEN + Fore.WHITE + "************************" + Back.RESET)
+    print(Back.GREEN + Fore.WHITE + "******* Clientes *******" + Back.RESET)
+    print(Back.GREEN + Fore.WHITE + "************************" + Back.RESET)
+
+    # Mostrar los clientes en pantalla
+    for cliente in clientes:
+        print(f"ID: {cliente['id']}, Nombre: {cliente['nombre']}")
+
+    # Solicitar al usuario que ingrese el ID del cliente comprador
+    cliente_id = int(input("Ingrese el ID cliente: "))
+
+    print(Back.GREEN + Fore.WHITE + "************************" + Back.RESET)
+    print(Back.GREEN + Fore.WHITE + "*** Productos Venta ****" + Back.RESET)
+    print(Back.GREEN + Fore.WHITE + "************************" + Back.RESET)
+
+    # Mostrar los productos en pantalla
+    for producto in productos:
+        print(f"ID: {producto['id']}, Nombre: {producto['nombre']}, Precio: {producto['precio']}, Stock: {producto['stock']}")
+
+    # Solicitar al usuario que ingrese el ID del producto a comprar
+    producto_id = int(input("Ingrese el ID del producto a comprar: "))
+
+    # Solicitar al usuario que ingrese cantidad a comprar
+    while cantOk:
+        print(Back.GREEN + Fore.WHITE + "************************" + Back.RESET)
+        producto_cant = input("Ingrese cantidad: ")
+        print(valida_numero(producto_cant))
+        if not valida_numero(producto_cant):
+            print('Debes ingresar un número')
+            continue
+        else:
+            cantOk = False
+    # Función para validar stock
+    os.system("cls" if os.name == "nt" else "clear")
+    if valida_stock(producto_id, producto_cant):
+        print('Compra aprobada y en camino!!')
+    else:
+        print('Compra cancelada: no hay suficiente stock para satisfacer el pedido!!')
+    time.sleep(3)
+    os.system("cls" if os.name == "nt" else "clear")
